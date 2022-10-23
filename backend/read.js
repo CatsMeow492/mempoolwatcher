4205242f551fac2b11e95e2afa0d87dfb9394dc5
CatsMeow492 <calisurf243@hotmail.com>
2022-10-23 10:59:11 -0700

    dynamodb setup - added dotenv

---
+++ backend/read.js
@@ -0,0 +1,27 @@
+var AWS = require('aws-sdk');
+require('dotenv').config()
+// set up aws config
+let awsConfig = {
+    "region": "us-east-1",
+    "endpoint": "http://dynamodb.us-east-1.amazonaws.com",
+    "accessKeyId": process.env.AWS_ACCESSKEYID, "secretAccessKey": process.env.AWS_SECRETACCESSKEY
+};
+AWS.config.update(awsConfig);
+// create a document client
+let docClient = new AWS.DynamoDB.DocumentClient();
+// read a single item
+let fetchOneByKey = function () {
+    let params = {
+        TableName: "users",
+        Key: {
+            "email_id": "example@example.com"
+        }
+    };
+    docClient.get(params, function (err, data) {
+        if (err) {
+            console.log("users::readSingleItem::error - " + JSON.stringify(err, null, 2));
+        } else {
+            console.log("users::readSingleItem::success - " + JSON.stringify(data, null, 2));
+        }
+    });
+}
